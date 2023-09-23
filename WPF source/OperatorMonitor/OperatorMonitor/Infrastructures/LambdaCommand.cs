822d0b66e9d64ac6fa9e428f2a9a4b1198a77d02
Hara-Ioshinory <hara.ioshinory@gmail.com>
2023-09-23 10:41:32 +0300

    Преднастройка WPF
    Добавление базовых классов MVVM

---
+++ WPF source/OperatorMonitor/OperatorMonitor/Infrastructures/LambdaCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+using System;
+
+namespace OperatorMonitor.Interstructures
+{
+    internal class LambdaCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+        public void Execute(object parameter) => _Execute(parameter);
+    }
+}
